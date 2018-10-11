daac81eceb737cecf1e74be45a06c415cd0b9cda
Aman Pandey <man22invisible@gmail.com>
2018-10-11 19:05:29 +0530

    extended application.py and added index.html

--- application.py
+++ application.py
@@ -1,8 +1,16 @@
-from flask import Flask
-
+from flask import Flask, redirect, render_template, url_for
+import os
 
 app = Flask(__name__)
 
+app_root = os.path.dirname(os.path.abspath(__file__))
+
 @app.route('/')
 def index():
-	return "Hello"
+	#return app_root
+	return render_template("index.html")
+
+@app.route('/send', methods=['POST'])
+def send():
+	target = os.path.join(app_root, "files/")
+	return target
